90801519e78b3c060afd8459b112032ab0c0c0a6
BAHMAN <imbahmanranjbar@gmail.com>
2025-01-26 12:03:53 +0330

    Add CreateArticleModel

---
+++ Models/CreateArticle.cs
@@ -0,0 +1,13 @@
+﻿namespace Razor_Blog.Models
+{
+    public class CreateArticle
+    {
+        public string Title { get; set; }
+        public string Picture { get; set; }
+        public string PictureAlt { get; set; }
+        public string PictureTitle  { get; set; }
+        public string ShortDescription { get; set; }
+        public string Body { get; set; }
+
+    }
+}
