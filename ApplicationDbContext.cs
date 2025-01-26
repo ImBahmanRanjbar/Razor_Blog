2b22a3ed926f1cfdf834295802e54f53fd9ce1d0
BAHMAN <imbahmanranjbar@gmail.com>
2025-01-26 11:52:12 +0330

    Add DbContext&packages

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Razor_Blog.Mapping;
+using Razor_Blog.Models;
+
+namespace Razor_Blog
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+
+        public DbSet<Article>   articles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ArticleMapping());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
